291727b28cf4f0fad1aea6ba9fa53021854057fb
konstantinrjab <konstantinrjab@gmail.com>
2020-04-21 23:52:40 +0300

    added migration

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ```shell script
 docker-compose up -d
-docker exec -it pok_python python manage.py db init
+docker exec -it pok_python python manage.py db upgrade
 ```
--- web/config.py
+++ web/config.py
@@ -7,12 +7,8 @@ class Config(object):
     """Base config, uses staging database server."""
     DEBUG = False
     TESTING = False
-    DB_SERVER = 'mysql'
-
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        return 'mysql://{user}:{password}@{server}/{database}'.format(user='root', password='root', server='mysql',
-                                                                      database='poker')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@mysql/poker'
 
 
 class DevelopmentConfig(Config):
